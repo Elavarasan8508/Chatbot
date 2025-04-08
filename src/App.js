221588f4e15962af345794f14bc4c221d4a22d82
Elavarasan8508 <elavarasan723892@gmail.com>
2025-04-08 18:04:19 +0530

    changes in index and app.js also

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/Dashboard";
+import { useLocation } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Navigate to="/register" />} />
-    <Route path="/register" element={<RegisterPage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/dashboard" element={<DashboardPage />} />
-  </Routes>
-</BrowserRouter>
-
+      <HashRouter>
+      <RouteDebugger />
+        <Routes>
+          <Route path="/" element={<RegisterPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
 
+function RouteDebugger() {
+  const location = useLocation();
+  console.log("Current route:", location.pathname);
+  return null;
+}
 export default App;
